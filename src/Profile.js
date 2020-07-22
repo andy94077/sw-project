25ff5ac0ede63e4ece7c449bd02bf3071612b901
Howard Tang <howard.tang@funpodium.net>
2020-07-22 10:50:05 +0800

    Adjust css

--- src/Profile.js
+++ src/Profile.js
@@ -3,67 +3,77 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
-  Avatar: {
+  Central: {
     display: "block",
     margin: "auto",
-    marginTop: "50px",
+  },
+  Center: {
+    textAlign: "center",
+  },
+  Rounded: {
+    marginTop: "20px",
     width: "120px",
     borderRadius: "60px",
   },
+  Bold: {
+    color: "#111",
+    fontWeight: "700",
+  },
   Name: {
-    textAlign: "center",
     fontSize: "36px",
   },
   Text: {
-    textAlign: "center",
     lineHeight: "25px",
+    fontSize: "16px",
   },
   URL: {
-    color: "#111",
-    fontSize: "16px",
-    fontWeight: "700",
+    textDecoration: "none",
     "&:hover": {
       textDecoration: "underline",
       cursor: "pointer",
     },
   },
-  Follow: {
-    color: "#111",
-    fontSize: "16px",
-    fontWeight: "700",
-  },
   Tmp: {
     marginTop: "100px",
-    textAlign: "center",
     fontSize: "21px",
     color: "#a3a19a",
   },
 });
 
-function Info(props) {
+function Profile(props) {
   const classes = useStyles();
   const { avatar, name, url, intro, follow } = props;
   return (
     <div>
-      <img alt="Avatar" className={classes.Avatar} src={avatar} />
-      <h2 className={classes.Name}>{name}</h2>
-      <div className={classes.Text}>
-        <span className={classes.URL} href={url}>
+      <img
+        alt="Avatar"
+        className={`${classes.Central} ${classes.Rounded}`}
+        src={avatar}
+      />
+      <h2 className={`${classes.Center} ${classes.Name}`}>{name}</h2>
+      <div className={`${classes.Center} ${classes.Text}`}>
+        <a className={`${classes.Bold} ${classes.URL}`} href={url}>
           {url}
-        </span>
+        </a>
         &nbsp;·&nbsp;
         <span>{intro}</span>
         <br />
-        <span className={classes.Follow}>
+        <span className={classes.Bold}>
           {follow[0]} followers · {follow[1]} following
         </span>
       </div>
-      <Button className={classes.Avatar} variant="contained" color="secondary">
+      <Button
+        className={`${classes.Central} ${classes.Rounded} ${classes.Text}`}
+        variant="contained"
+        color="secondary"
+      >
         Follow
       </Button>
-      <div className={classes.Tmp}>No image on this profile.</div>
+      <div className={`${classes.Center} ${classes.Tmp}`}>
+        No image on this profile.
+      </div>
     </div>
   );
 }
 
-export default Info;
+export default Profile;
