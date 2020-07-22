89f1accfb5e1cf2aaf4bad0ee22d61ea90648bb3
andy.chen <andy.chen@funpodium.net>
2020-07-22 15:34:05 +0800

    Add bar to Photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { GridListTileBar, makeStyles } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+
+const useStyles = makeStyles({
+  root: {
+    position: "relative",
+  },
+  icon: {
+    color: "#eeeeee",
+  },
+});
 
 export default function Photo(props) {
+  const classes = useStyles();
+  const [barVisibility, setBarVisibility] = useState(false);
   const { image, src } = props;
 
+  const changeBarVisibility = () => setBarVisibility(!barVisibility);
+
   return (
-    <img src={src} alt={image} style={{ width: "100%", height: "auto" }} />
+    <div
+      className={classes.root}
+      onMouseEnter={changeBarVisibility}
+      onMouseLeave={changeBarVisibility}
+    >
+      <img src={src} alt={image} style={{ width: "100%", height: "auto" }} />
+      {barVisibility && (
+        <GridListTileBar
+          title={image}
+          actionIcon={
+            <IconButton
+              aria-label={`info about ${image}`}
+              className={classes.icon}
+            >
+              <InfoIcon />
+            </IconButton>
+          }
+        />
+      )}
+    </div>
   );
 }
