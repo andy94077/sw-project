e2ef9d5d40b3addee9da3565d19a0178a4e28645
andy.chen <andy.chen@funpodium.net>
2020-07-22 16:45:55 +0800

    Finish PhotoGrid.js

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -7,7 +7,7 @@ export default function PhotoGrid(props) {
   const { imageList } = props;
 
   const photos = imageList.map((image) => (
-    <Photo key={image} image={image} src={`images/${image}`} cols={1} />
+    <Photo key={image} image={image} src={`images/${image}`} />
   ));
 
   return <div className="grid">{photos}</div>;
