03bc487e953c81ee62ae8b6ffd0915e7765d42e5
silencespeaker755 <b06902019@ntu.edu.tw>
2020-07-23 17:13:47 +0800

    Finish basic form

--- src/Login/FormInfo.js
+++ src/Login/FormInfo.js
@@ -1,7 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
+import Loading from "../components/Loading";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,6 +30,63 @@ export default function FormInfo() {
   const username = useRef();
   const password = useRef();
 
+  const [state, setState] = useState({
+    username: "",
+    isError: false,
+    nowLoading: false,
+    errorMes: "",
+  });
+
+  const handleSubmit = () => {
+    setState({
+      username: username.current.value,
+    });
+    // Check if it is a valid input
+    //
+    setState({
+      isError: state.isError,
+      nowLoading: true,
+      errorMes: state.errorMes,
+    });
+
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+    const boolTest = false;
+    const formdata = new FormData();
+    formdata.append("username", username.current.value);
+    formdata.append("password", password.current.value);
+    axios
+      .post("http://localhost:3000", formdata, config)
+      .then((response) => {
+        if (response === null) return;
+        if (boolTest !== true) {
+          alert("Message already sent!");
+          setState({
+            isError: false,
+            nowLoading: false,
+          });
+        } else {
+          setState({
+            isError: true,
+            nowLoading: false,
+            errorMes: "username or password is not found",
+          });
+        }
+      })
+      .catch((error) => {
+        if (error === null) return;
+        setState({
+          isError: true,
+          nowLoading: false,
+          errorMes: "connection fail",
+        });
+      });
+  };
+
   return (
     <div className={classes.centerMargin}>
       <form className={classes.root} noValidate autoComplete="off">
@@ -36,6 +95,8 @@ export default function FormInfo() {
           label="User Name"
           variant="outlined"
           required="true"
+          error={state.isError}
+          helperText={state.errorMes}
           placeholder="enter your username"
           color="primary"
           className={classes.controlSpace}
@@ -43,19 +104,30 @@ export default function FormInfo() {
         />
 
         <TextField
+          type="password"
           inputRef={password}
           label="Password"
           variant="outlined"
           required="true"
+          error={state.isError}
+          helperText={state.errorMes}
           placeholder="enter your password"
           color="primary"
           className={classes.controlSpace}
           InputProps={{ style: { borderRadius: "50px" } }}
         />
 
-        <Button variant="contained" className={classes.controlButton}>
-          Continue
-        </Button>
+        {state.nowLoading ? (
+          <Loading />
+        ) : (
+          <Button
+            variant="contained"
+            className={classes.controlButton}
+            onClick={handleSubmit}
+          >
+            Continue
+          </Button>
+        )}
       </form>
     </div>
   );
