4ffd95ec67c12e5ebd6e571f8ef5ab0e3af69a07
b07902011 <b07902011@ntu.edu.tw>
2020-07-23 10:05:03 +0800

    Adjust Conent file name and conent code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box } from "@material-ui/core";
-import Content from "./content/Content";
+import Content from "./Content/Content";
 
 function App() {
   return (
