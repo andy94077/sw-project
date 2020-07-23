6f5f38c761d4d704f06bbf3072dcf8b897904f28
andy.chen <andy.chen@funpodium.net>
2020-07-23 14:56:38 +0800

    Link each component in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
+import HomePage from "./Homepage/Homepage";
+import LoginPage from "./Login/LoginPage";
+import Content from "./Content/Content";
+import Profile from "./Profile/Profile";
+import Bar from "./Bar/Bar";
 
 function App() {
-  return <h2>Hello World!</h2>;
+  const [tabIndex, setTabIndex] = useState("Homepage");
+
+  setTabIndex("Homepage");
+
+  let currentPage;
+  switch (tabIndex) {
+    case "LoginPage":
+      currentPage = <LoginPage />;
+      break;
+    case "Homepage":
+      currentPage = (
+        <HomePage
+          imageList={Array.from({ length: 12 }, (_, i) => `${i + 1}.jpg`)}
+        />
+      );
+      break;
+    case "Content":
+      currentPage = <Content />;
+      break;
+    case "Profile":
+      currentPage = <Profile />;
+      break;
+    default:
+      currentPage = <LoginPage />;
+      break;
+  }
+  return (
+    <div>
+      <Bar avatar="pictures/avatar.jpg" />
+      {currentPage}
+    </div>
+  );
 }
 
 export default App;
