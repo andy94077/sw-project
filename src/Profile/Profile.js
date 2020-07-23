9ead7fafc8d8a530408be977f297fb889c03a7e9
andy.chen <andy.chen@funpodium.net>
2020-07-23 17:22:02 +0800

    Rename profile/ to Profile/

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
-import Bar from "../bar/Bar";
 
 const useStyles = makeStyles({
   central: {
@@ -46,7 +45,6 @@ export default function Profile(props) {
   const { avatar, name, url, intro, follow } = props;
   return (
     <div>
-      <Bar avatar="pictures/avatar.jpeg" />
       <img
         alt="Avatar"
         className={`${classes.central} ${classes.rounded}`}
