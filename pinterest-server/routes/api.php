e337ce92ec2f0032dc593240da5da92dba6b5272
b07902011 <b07902011@ntu.edu.tw>
2020-07-30 10:39:37 +0800

    Fix some async warning

--- pinterest-server/routes/api.php
+++ pinterest-server/routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('Api')->prefix('v1')->group(function () {
     Route::get('/comments', 'CommentController@index');
-    Route::get('/comment/{post}', 'CommentController@showByPost');
+    Route::get('/comment/post', 'CommentController@showByPost');
     Route::post('/comment/upload', 'CommentController@upload');
     Route::apiResource('comment', 'CommentController');
 
