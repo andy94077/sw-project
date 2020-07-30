4fcd1d5d10dbf30b4e0ff44d524a03036136d9cc
andy.chen <andy.chen@funpodium.net>
2020-07-30 16:47:47 +0800

    Add "No Image Found" feature in PhotoGrid

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
 
 import Loading from "./Loading";
 import Photo from "./Photo";
 import "./PhotoGrid.css";
 
+const useStyles = makeStyles(() => ({
+  loading: {
+    marginTop: "40px",
+  },
+  noImage: {
+    marginTop: "80px",
+    color: "#707070",
+  },
+}));
+
 export default function PhotoGrid(props) {
   const { tag, userId, number = 120 } = props;
+  const classes = useStyles();
   const [isReady, setIsReady] = useState(false);
   const [imageListWithid, setImageListWithId] = useState();
 
@@ -37,5 +50,24 @@ export default function PhotoGrid(props) {
     ));
   }
 
-  return <div className="grid">{isReady ? photos : <Loading />}</div>;
+  if (isReady) {
+    if (imageListWithid.length === 0) {
+      return (
+        <Typography
+          className={classes.noImage}
+          align="center"
+          variant="h4"
+          display="block"
+        >
+          No Images Found
+        </Typography>
+      );
+    }
+    return <div className="grid">{photos}</div>;
+  }
+  return (
+    <div className={classes.loading}>
+      <Loading />
+    </div>
+  );
 }
