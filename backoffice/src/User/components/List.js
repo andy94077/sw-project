dcb6fd4e55c8c4539e1abab2ad2dfb5facaf4bc5
silencespeaker755 <b06902019@ntu.edu.tw>
2020-08-05 11:48:04 +0800

    Add new column

--- backoffice/src/User/components/List.js
+++ backoffice/src/User/components/List.js
@@ -30,38 +30,65 @@ export default function List(props) {
 
   const columns = [
     {
-      title: "Id",
+      title: "Avatar",
+      dataIndex: "avatar_url",
+      key: "avatar",
+      width: 80,
+      //fixed: "left",
+      render: (text) => (
+        <Avatar style={{ marginLeft: 8 }} src={CONCAT_SERVER_URL(text)} />
+      ),
+    },
+    {
+      title: "ID",
       dataIndex: "id",
       key: "id",
+      width: 70,
+      sorter: (a, b) => a.id - b.id,
     },
     {
       title: "Name",
       dataIndex: "name",
       key: "name",
+      width: 120,
       render: (text, record) => <Link to={`user/${record.id}`}>{text}</Link>,
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
       title: "Email",
       dataIndex: "email",
       key: "email",
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
-      title: "Avatar",
-      dataIndex: "avatar_url",
-      key: "avatar",
-      //width: 72,
-      //fixed: "left",
-      render: (text) => <Avatar style={{ marginLeft: 8 }} src={text} />,
+      title: "Online Time",
+      dataIndex: "online_time",
+      key: "online_time",
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: "Bucket Time",
+      dataIndex: "bucket_time",
+      key: "bucket_time",
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
       title: "created_at",
       dataIndex: "created_at",
       key: "created_at",
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
-      title: "updated_at",
-      dataIndex: "updated_at",
-      key: "updated_at",
+      title: "delete_at",
+      dataIndex: "delete_at",
+      key: "delete_at",
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: "update_at",
+      dataIndex: "update_at",
+      key: "update_at",
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
       title: "Operation",
