6d049397b5ebd05ec02f13e09e534e95bc96a499
andy.chen <andy.chen@funpodium.net>
2020-08-11 10:09:48 +0800

    Refactor App.js

--- backoffice/src/App.js
+++ backoffice/src/App.js
@@ -1,27 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Layout, Menu } from "antd";
-import "./App.css";
-import {
-  PieChartOutlined,
-  TeamOutlined,
-  FileTextOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import { Switch, Route, useLocation, useHistory, Link } from "react-router-dom";
+import { Switch, Route, useLocation, useHistory } from "react-router-dom";
 
 import Post from "./Post/Post";
 import User from "./User/User";
 import LoginPage from "./Login/LoginPage";
 import BOUser from "./BOUser/BOUser";
+import Dashboard from "./Dashboard/Dashboard";
 import ErrorMsg from "./components/ErrorMsg";
 import Loading from "./components/Loading";
+import Bar from "./Bar/Bar";
 
 import { getCookie, deleteCookie } from "./cookieHelper";
 import { CONCAT_SERVER_URL } from "./constants";
-import Dashboard from "./Dashboard/Dashboard";
-
-const { Header, Content, Footer, Sider } = Layout;
 
 export default function App() {
   const [user, setUser] = useState({ username: null, userId: null });
@@ -59,11 +50,6 @@ export default function App() {
     }
   }, [location, history]);
 
-  const logOut = () => {
-    deleteCookie();
-    history.push("/");
-  };
-
   if (isReady) {
     if (error.message !== "") {
       return <ErrorMsg message={error.message} imgUrl={error.url} />;
@@ -76,61 +62,17 @@ export default function App() {
       );
     }
     return (
-      <Layout className="base-layout">
-        <Header className="header">
-          <div className="logo">sw-project-backoffice</div>
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            className="horizontal-menu"
-            selectable={false}
-            triggerSubMenuAction="click"
-          >
-            <Menu.SubMenu icon={<UserOutlined />} title={user.username}>
-              <Menu.Item onClick={logOut}>Log out</Menu.Item>
-            </Menu.SubMenu>
-          </Menu>
-        </Header>
-        <Layout className="site-layout">
-          <Sider collapsible>
-            <Menu
-              theme="dark"
-              defaultSelectedKeys={[location.pathname.split("/").slice(-1)[0]]}
-              mode="inline"
-            >
-              <Menu.Item
-                key="dashboard"
-                icon={<PieChartOutlined />}
-                className="first-menu-item"
-              >
-                <Link to={"/"}>Dashboard</Link>
-              </Menu.Item>
-              <Menu.Item key="user" icon={<TeamOutlined />}>
-                <Link to={"/user"}>Users</Link>
-              </Menu.Item>
-              <Menu.Item key="post" icon={<FileTextOutlined />}>
-                <Link to={"/post"}>Posts</Link>
-              </Menu.Item>
-              <Menu.Item key="BOUser" icon={<TeamOutlined />}>
-                <Link to={"/BOUser"}>BO Users</Link>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout>
-            <Content className="content">
-              <Switch>
-                <Route exact path={"/dashboard"} component={Dashboard} />
-                <Route exact path={"/user"} component={User} />
-                <Route exact path={"/post"} component={Post} />
-                <Route exact path={"/BOUser"} component={BOUser} />
-              </Switch>
-            </Content>
-            <Footer className="footer">
-              sw-project ©2020 Created by Funpodium
-            </Footer>
-          </Layout>
-        </Layout>
-      </Layout>
+      <Bar
+        username={user.username}
+        content={
+          <Switch>
+            <Route exact path={"/dashboard"} component={Dashboard} />
+            <Route exact path={"/user"} component={User} />
+            <Route exact path={"/post"} component={Post} />
+            <Route exact path={"/BOUser"} component={BOUser} />
+          </Switch>
+        }
+      />
     );
   }
   return <Loading />;
