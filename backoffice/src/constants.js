c69f0d25e12df712a38fda22c6ef404227ea6d6e
silencespeaker755 <b06902019@ntu.edu.tw>
2020-08-17 10:04:46 +0800

    Remove local constants.js

--- backoffice/src/constants.js
+++
@@ -1,3 +0,0 @@
-export const SERVER_URL = "http://pinterest-server.test";
-export const REDIS_URL = `${SERVER_URL}:6001`;
-export const BACKOFFICE_URL = "/backoffice";
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const SERVER_URL = "http://pinterest-server.test";
-export const REDIS_URL = `${SERVER_URL}:6001`;
