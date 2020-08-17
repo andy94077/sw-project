009bc04135ac8726f2ccb2cbda88f114ff8d94b4
b07902011 <b07902011@ntu.edu.tw>
2020-08-17 16:00:11 +0800

    Add Post component

--- src/Content/ContentCard.js
+++ src/Content/ContentCard.js
@@ -1,34 +1,21 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
-import Axios from "axios";
 import {
   Collapse,
-  Button,
   CardMedia,
   CardContent,
   Card,
-  CardHeader,
   IconButton,
-  Menu,
-  MenuItem,
 } from "@material-ui/core";
 import CardActions from "@material-ui/core/CardActions";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import { useDispatch, useSelector } from "react-redux";
 
-import { Link, Redirect } from "react-router-dom";
-import Loading from "../components/Loading";
-import { CONCAT_SERVER_URL } from "../constants";
-import AlertDialog from "../components/AlertDialog";
-import MyQuill from "../components/MyQuill";
 import ErrorMsg from "../components/ErrorMsg";
-import { setDialog } from "../redux/dialogSlice";
 import "./Content.css";
 import Like from "./Like";
-import { selectUser } from "../redux/userSlice";
 import Comment from "./Comment";
+import Post from "./Post";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -123,34 +110,6 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
   },
-  author: {
-    height: "40px",
-  },
-  text: {
-    overflow: "auto",
-    flexGrow: "1",
-  },
-  time: {
-    overflow: "hidden",
-    flexGrow: "1",
-  },
-  cardHeader: {
-    padding: 0,
-  },
-  action: {
-    margin: 0,
-  },
-  subheader: {
-    fontWeight: 500,
-    fontSize: "0.875em",
-  },
-  user: {
-    flexGrow: "1",
-  },
-  myQuill: {
-    marginRight: "5%",
-    width: "90%",
-  },
 }));
 
 export default function ContentCard(props) {
@@ -159,222 +118,63 @@ export default function ContentCard(props) {
   const [error, setError] = useState({ message: "", url: "" });
   const [expand, setExpand] = useState(true);
   const [isCoverOpen, setIsCoverOpen] = useState(false);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [menu, setMenu] = useState(false);
-  const [onDelete, setOnDelete] = useState(0);
-  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
-  const [onEdit, setOnEdit] = useState(0);
-  const [newPost, setNewPost] = useState(content);
-  const { username, userId } = useSelector(selectUser);
-
-  const dispatch = useDispatch();
-
-  const handleClick = (event) => {
-    setMenu(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setMenu(null);
-  };
-
-  const handleDelete = () => {
-    setOnDelete(1);
-    Axios.delete(CONCAT_SERVER_URL("/api/v1/post"), {
-      data: { id },
-    })
-      .then(() => {
-        setOnDelete(2);
-      })
-      .catch((e) => {
-        if (e.message === "Network Error") {
-          dispatch(
-            setDialog({
-              title: "Network Error",
-              message: "Failed to delete post, pleace retry",
-            })
-          );
-        }
-        setOnDelete(0);
-      })
-      .finally(() => {
-        setIsDialogOpen(false);
-      });
-  };
-
-  const handleDialogClose = () => {
-    setIsDialogOpen(false);
-  };
-
-  const handleEditDialogClose = () => {
-    setIsEditDialogOpen(false);
-  };
-
-  async function handleEdit() {
-    if (onEdit === 0) {
-      setOnEdit(1);
-      Axios.post(CONCAT_SERVER_URL("/api/v1/post/modification"), {
-        id,
-        content: newPost,
-        user: true,
-        user_id: userId,
-      })
-        .then(() => {
-          setIsEditDialogOpen(false);
-          refresh();
-        })
-        .catch(() => {
-          setError({
-            message: "Connection Error",
-            url: "/pictures/connection-error.svg",
-          });
-        })
-        .finally(() => {
-          setOnEdit(0);
-        });
-    }
+  if (error.message !== "") {
+    return <ErrorMsg message={error.message} imgUrl={error.url} />;
   }
-
-  if (onDelete === 1) {
-    return <Loading />;
-  }
-  if (onDelete === 2) {
-    return <Redirect to="/home" />;
-  }
-  if (onDelete === 0) {
-    if (error.message !== "") {
-      return <ErrorMsg message={error.message} imgUrl={error.url} />;
-    }
-    return (
-      <Card className={classes.root}>
-        <CardMedia
-          className={clsx(classes.cover, {
-            [classes.coverOpen]: isCoverOpen,
-          })}
-          image={src}
-          title="Live from space album cover"
-          onClick={() => {
-            setIsCoverOpen(!isCoverOpen);
-          }}
-        />
-        <div className={classes.details}>
-          <CardContent className={classes.content}>
-            <CardHeader
-              classes={{
-                root: classes.cardHeader,
-                action: classes.action,
-                subheader: classes.subheader,
-              }}
-              title={
-                <Link to={`/profile/${author}`} className={classes.user}>
-                  {author}
-                </Link>
-              }
-              subheader={timeAgo}
-              action={
-                author === username && (
-                  <>
-                    <IconButton
-                      size="small"
-                      onClick={handleClick}
-                      aria-controls="m"
-                    >
-                      <MoreVertIcon />
-                    </IconButton>
-                    <Menu
-                      id="m"
-                      anchorEl={menu}
-                      keepMounted
-                      open={Boolean(menu)}
-                      onClose={handleClose}
-                    >
-                      <MenuItem
-                        onClick={() => {
-                          setMenu(false);
-                          setIsDialogOpen(true);
-                        }}
-                      >
-                        delete
-                      </MenuItem>
-                      <MenuItem
-                        onClick={() => {
-                          setMenu(false);
-                          setIsEditDialogOpen(true);
-                        }}
-                      >
-                        Edit
-                      </MenuItem>
-                    </Menu>
-                    <AlertDialog
-                      open={isDialogOpen}
-                      alertTitle="Warning"
-                      alertDesciption="Do you really want to delete this post?"
-                      alertButton={
-                        <>
-                          <Button onClick={handleDelete}>Yes</Button>
-                          <Button onClick={handleDialogClose}>No</Button>
-                        </>
-                      }
-                      onClose={handleDialogClose}
-                    />
-                    <AlertDialog
-                      open={isEditDialogOpen}
-                      alertTitle="Edit Post"
-                      alertButton={
-                        <>
-                          <Button onClick={handleEdit}>Yes</Button>
-                          <Button onClick={handleEditDialogClose}>No</Button>
-                        </>
-                      }
-                      onClose={handleEditDialogClose}
-                      moreComponent={
-                        <MyQuill
-                          className={classes.myQuill}
-                          value={newPost}
-                          setValue={setNewPost}
-                        />
-                      }
-                    />
-                  </>
-                )
-              }
-            />
-            <div
-              className={classes.text}
-              dangerouslySetInnerHTML={{ __html: content }}
-            />
-          </CardContent>
-          <CardActions disableSpacing className={classes.cardActions}>
-            <Like id={id} />
-            <IconButton
-              component="span"
-              onClick={() => {
-                setExpand(!expand);
-              }}
-              className={clsx(classes.expand, {
-                [classes.expandOpen]: expand,
-              })}
-              size={expand ? "small" : "medium"}
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </CardActions>
-          <Collapse
-            in={expand}
-            classes={{
-              container: classes.collapse,
-              wrapper: classes.wrapper,
-              wrapperInner: classes.wrapperInner,
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={clsx(classes.cover, {
+          [classes.coverOpen]: isCoverOpen,
+        })}
+        image={src}
+        title="Live from space album cover"
+        onClick={() => {
+          setIsCoverOpen(!isCoverOpen);
+        }}
+      />
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Post
+            author={author}
+            timeAgo={timeAgo}
+            content={content}
+            id={id}
+            setError={setError}
+            refresh={refresh}
+          />
+        </CardContent>
+        <CardActions disableSpacing className={classes.cardActions}>
+          <Like id={id} />
+          <IconButton
+            component="span"
+            onClick={() => {
+              setExpand(!expand);
             }}
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expand,
+            })}
+            size={expand ? "small" : "medium"}
           >
-            <Comment
-              author={author}
-              isBucket={isBucket}
-              id={id}
-              setError={setError}
-            />
-          </Collapse>
-        </div>
-      </Card>
-    );
-  }
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse
+          in={expand}
+          classes={{
+            container: classes.collapse,
+            wrapper: classes.wrapper,
+            wrapperInner: classes.wrapperInner,
+          }}
+        >
+          <Comment
+            author={author}
+            isBucket={isBucket}
+            id={id}
+            setError={setError}
+          />
+        </Collapse>
+      </div>
+    </Card>
+  );
 }
