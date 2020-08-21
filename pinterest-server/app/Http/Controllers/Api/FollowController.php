7ea6e86cfcfb146a96df0dfc5c9c84e2618cb453
andy.chen <andy.chen@funpodium.net>
2020-08-21 16:38:42 +0800

    Fix follows bug

--- pinterest-server/app/Http/Controllers/Api/FollowController.php
+++ pinterest-server/app/Http/Controllers/Api/FollowController.php
@@ -10,123 +10,132 @@ use App\Models\User;
 
 class FollowController extends BaseController
 {
-    public function index(Request $request){
-        if($request['follower_id'] != null && $request['target_id'] != null){
+    public function index(Request $request)
+    {
+        if ($request['follower_id'] != null && $request['target_id'] != null) {
             $followers = Follow::where('follower_id', $request['follower_id'])->where('target_id', $request['target_id'])->get();
-        }
-        else if($request['follower_id'] != null){
+        } else if ($request['follower_id'] != null) {
             $followers = Follow::where('follower_id', $request['follower_id'])->get();
-        }
-        else if($request['target_id'] != null){
+        } else if ($request['target_id'] != null) {
             $followers = Follow::where('target_id', $request['target_id'])->get();
-        }
-        else{
+        } else {
             $followers = Follow::all();
         }
         return response()->json($followers);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $follows = Follow::find($id);
         return response()->json($follows);
     }
 
-    public function store(Request $request){
-        if($request['follower_id'] === null || $request['target_id'] === null){
+    public function store(Request $request)
+    {
+        if ($request['follower_id'] === null || $request['target_id'] === null) {
             return response()->json("follower_id or target_id not found", 404);
         }
         $follows = Follow::withTrashed()->where('follower_id', $request['follower_id'])->where('target_id', $request['target_id'])->get();
-        if($follows->count() !== 0){
+        if ($follows->count() !== 0) {
             $follows = $follows[0];
-            if($follows->deleted_at !== null){
+            if ($follows->deleted_at !== null) {
                 $follows->restore();
             }
-        }
-        else{
+        } else {
             $follows = new Follow();
             $follows->follower_id = $request['follower_id'];
             $follows->target_id = $request['target_id'];
             $follows->save();
         }
         $follower = User::find($request['follower_id']);
-        $follower->followings ++;
+        $follower->followings++;
         $follower->save();
         $target = User::find($request['target_id']);
-        $target->followers ++;
+        $target->followers++;
         $target->save();
         return response()->json($follows);
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
         $follow = Follow::find($id);
         $follower = User::find($follow->follower_id);
-        $follower->followings --;
+        $follower->followings--;
         $follower->save();
+
         $target = User::find($follow->target_id);
-        $target->followers --;
+        $target->followers--;
         $target->save();
+
         $follow->delete();
         return response()->json($follow);
     }
 
-    public function destroyByUser(Request $request){
+    public function destroyByUser(Request $request)
+    {
         $follow = Follow::where('follower_id', $request['follower_id'])->where('target_id', $request['target_id']);
-        return destroy($follow->id);
+        return $this->destroy($follow->id);
     }
 
-    public function update($id){
+    public function update($id)
+    {
         $follow = Follow::withTrashed()->find($id);
-        $follower = User::find($request['follower_id']);
-        $follower->followings ++;
+        $follower = User::find($follow->follower_id);
+        $follower->followings++;
         $follower->save();
-        $target = User::find($request['target_id']);
-        $target->followers ++;
+        $target = User::find($follow->target_id);
+        $target->followers++;
         $target->save();
         $follow->restore();
         return response()->json($follow);
     }
 
-    public function updateByUser(Request $request){
+    public function updateByUser(Request $request)
+    {
         $follow = Follow::withTrashed()->where('follower_id', $request['follower_id'])->where('target_id', $request['target_id']);
-        return update($follow->id);
+        return $this->update($follow->id);
     }
 
-    public function getFollowInfo(Request $request){
-        $res['followers'] = Follow::where('target_id', $request['user_id'])->count();
-        $res['followings'] = Follow::where('follower_id', $request['user_id'])->count();
-        return response()->json($res);
+    public function getFollowInfo(Request $request)
+    {
+        $user = User::find($request['user_id']);
+        return response()->json(['followers' => $user->followers, 'followings' => $user->followings]);
     }
 
-    public function getFollowing(Request $request){
+    public function getFollowing(Request $request)
+    {
         $user = User::where('name', $request['name'])->first();
         $user_id = $user->id;
         $slex = Follow::LeftJoin('users', 'users.id', '=', 'follows.follower_id')->where('target_id', $user_id)->select('users.name as username', 'users.avatar_url as avatar_url');
         $followings = $slex->skip(($request['nextId']) * 10)->take(10)->get();
-        if(count($followings) > 0){
-            return response()->json(["message" => $followings, 'nextId' => $request['nextId']+1], 200);
+        if (count($followings) > 0) {
+            return response()->json(["message" => $followings, 'nextId' => $request['nextId'] + 1], 200);
         } else {
             return response()->json(["message" => $followings, 'nextId' => false], 200);
         }
     }
 
-    public function getFollower(Request $request){
+    public function getFollower(Request $request)
+    {
         $user = User::where('name', $request['name'])->first();
         $user_id = $user->id;
         $slex = Follow::LeftJoin('users', 'users.id', '=', 'follows.target_id')->where('follower_id', $user_id)->select('users.name as username', 'users.avatar_url as avatar_url');
         $followings = $slex->skip(($request['nextId']) * 10)->take(10)->get();
-        if(count($followings) > 0){
-            return response()->json(["message" => $followings, 'nextId' => $request['nextId']+1], 200);
+        if (count($followings) > 0) {
+            return response()->json(["message" => $followings, 'nextId' => $request['nextId'] + 1], 200);
         } else {
             return response()->json(["message" => $followings, 'nextId' => false], 200);
         }
     }
 
-    public function getFollowingAdmin(Request $request){
+    public function getFollowingAdmin(Request $request)
+    {
         $followings = Follow::withTrashed()->LeftJoin('users', 'users.id', '=', 'follows.follower_id')->where('target_id', $request['user_id'])->select('users.*')->get();
         return response()->json($followings);
     }
 
-    public function getFollowerAdmin(Request $request){
+    public function getFollowerAdmin(Request $request)
+    {
         $followings = Follow::withTrashed()->LeftJoin('users', 'users.id', '=', 'follows.target_id')->where('follower_id', $request['user_id'])->select('users.*')->get();
         return response()->json($followings);
     }
