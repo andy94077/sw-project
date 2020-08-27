537dd94f83d95699881e749240bef4832365ee54
b07902011 <b07902011@ntu.edu.tw>
2020-08-27 16:29:35 +0800

    Add verify

--- pinterest-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ pinterest-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,6 +27,7 @@ class CreateUsersTable extends Migration
             $table->string('intro')->nullable();
             $table->unsignedBigInteger('followers')->default(0);
             $table->unsignedBigInteger('followings')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
