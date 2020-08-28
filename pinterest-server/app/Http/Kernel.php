f9a0902ae87ee0333a5ddd2f2ae6bd9566c62bf5
b07902011 <b07902011@ntu.edu.tw>
2020-08-28 10:15:32 +0800

    Fix api

--- pinterest-server/app/Http/Kernel.php
+++ pinterest-server/app/Http/Kernel.php
@@ -62,7 +62,8 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+        //'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'bucket' => \App\Http\Middleware\BucketMiddleware::class,
         'BO_can' => \App\Http\Middleware\AuthorizeBOMiddleware::class,
     ];
