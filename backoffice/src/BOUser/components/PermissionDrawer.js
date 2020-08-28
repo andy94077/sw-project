d466810e25a203c9d446e2de8b5a865f02ecb52a
andy.chen <andy.chen@funpodium.net>
2020-08-28 09:30:32 +0800

    Update css style

--- backoffice/src/BOUser/components/PermissionDrawer.js
+++ backoffice/src/BOUser/components/PermissionDrawer.js
@@ -1,14 +1,32 @@
 import React from "react";
 import { Drawer, Divider, Typography, Tag } from "antd";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  tag: {
+    borderRadius: 5,
+    marginBottom: 5,
+    "&:last-child": {
+      marginBottom: 0,
+    },
+  },
+}));
 
 export default function PermissionDrawer(props) {
   const { rolesWithPermissions, onClose, visible } = props;
+  const classes = useStyles();
+
   return (
     <Drawer
       width={400}
       getContainer={false}
-      title={<Typography.Title level={3}>Permissions</Typography.Title>}
+      title={
+        <Typography.Title level={3} style={{ marginBottom: 5 }}>
+          Permissions
+        </Typography.Title>
+      }
       placement="right"
+      headerStyle={{ paddingBottom: 0 }}
       closable={false}
       onClose={onClose}
       visible={visible}
@@ -18,14 +36,8 @@ export default function PermissionDrawer(props) {
           <Typography.Title level={4} key={role}>
             {role}
           </Typography.Title>
-          {permissions.map((item, index) => (
-            <Tag
-              style={{
-                borderRadius: 5,
-                marginBottom: index === permissions.length - 1 ? 0 : 5,
-              }}
-              key={item}
-            >
+          {permissions.map((item) => (
+            <Tag className={classes.tag} key={item}>
               {item}
             </Tag>
           ))}
