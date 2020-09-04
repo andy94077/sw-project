6178dd3b839eae6ee8ba9119c32b7dfdc9c65afa
Howard Tang <howard.tang@funpodium.net>
2020-09-04 15:25:07 +0800

    Change seeder

--- pinterest-server/database/seeds/ChatroomsTableSeeder.php
+++ pinterest-server/database/seeds/ChatroomsTableSeeder.php
@@ -19,7 +19,7 @@ class ChatroomsTableSeeder extends Seeder
                 'user_id1' => 2,
                 'user_id2' => 3,
                 'room_id' => 1,
-                'last_message' => "Hi 30 !",
+                'last_message' => "Hi 75 !",
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ]
@@ -29,7 +29,7 @@ class ChatroomsTableSeeder extends Seeder
                 'user_id1' => 3,
                 'user_id2' => 2,
                 'room_id' => 1,
-                'last_message' => "Hi 30 !",
+                'last_message' => "Hi 75 !",
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ]
@@ -41,14 +41,14 @@ class ChatroomsTableSeeder extends Seeder
             $table->string('message');
             $table->timestamps();
         });
-        for ($i = 1; $i <= 30; $i++) {
+        for ($i = 1; $i <= 75; $i++) {
             DB::table('chat_1')->insert(
                 [
                     'from' => 2 + $i % 2,
                     'to' => 3 - $i % 2,
                     'message' => "Hi " . $i . " !",
-                    'created_at' => date_sub(date_create(date('Y-m-d H:i:s')), date_interval_create_from_date_string(31 - $i . " minutes")),
-                    'updated_at' => date_sub(date_create(date('Y-m-d H:i:s')), date_interval_create_from_date_string(31 - $i . " minutes"))
+                    'created_at' => date_sub(date_create(date('Y-m-d H:i:s')), date_interval_create_from_date_string(76 - $i . " minutes")),
+                    'updated_at' => date_sub(date_create(date('Y-m-d H:i:s')), date_interval_create_from_date_string(76 - $i . " minutes"))
                 ]
             );
         }
